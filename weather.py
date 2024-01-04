b7c874d81448be3484b7ee753dfdb84acbcc9a3a
tikkikkit21 <tikkikkit21@gmail.com>
2024-01-04 18:36:41 -0500

    Add user input

--- weather.py
+++ weather.py
@@ -4,4 +4,11 @@ if __name__ == "__main__":
     print("Welcome to WeatherML!")
     print("Training model...")
     model.train()
-    model.predict(80, 80, 0.44, 9, 4)
+
+    temp = input("What is the actual temperature (°F)? ")
+    feelsLike = input("What is the feels like temperature (°F)? ")
+    humidity = input("What is the humidity (%)? ")
+    uv = input("What is the UV? ")
+    wind = input("What is the wind speed (mph)? ")
+
+    model.predict(temp, feelsLike, humidity, uv, wind)
