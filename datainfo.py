863fa97c0277bc21e514da9acc6ebb21551768b2
tikkikkit21 <tikkikkit21@gmail.com>
2024-01-05 10:22:16 -0500

    Add hist for time samples

--- datainfo.py
+++ datainfo.py
@@ -1,5 +1,6 @@
 import matplotlib.pyplot as plot
 import pandas as pd
+from datetime import datetime
 
 data = pd.read_csv("weather_data_v1.csv")
 
@@ -9,3 +10,11 @@ def label_hist():
 
     y.value_counts().sort_index().plot.bar(x="Label", y="# Occurences")
     plot.show()
+
+def time_freq():
+    times = data["Date/Time"].apply(lambda d: datetime.strptime(d, '%m/%d/%Y %H:%M'))
+    times = times.apply(lambda d: int(d.time().strftime('%H%M')))
+    times.plot.hist(bins=24)
+    plot.show()
+
+time_freq()
