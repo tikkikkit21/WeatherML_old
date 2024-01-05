8309ad26cf756a143ec41630a88391f670a1fee1
tikkikkit21 <tikkikkit21@gmail.com>
2024-01-05 10:06:04 -0500

    Move hist code to function

--- datainfo.py
+++ datainfo.py
@@ -3,8 +3,9 @@ import pandas as pd
 
 data = pd.read_csv("weather_data_v1.csv")
 
-x = data.iloc[:,:-1]
-y = data['Outer Clothing']
+def label_hist():
+    x = data.iloc[:,:-1]
+    y = data['Outer Clothing']
 
-y.value_counts().sort_index().plot.bar(x="Label", y="# Occurences")
-plot.show()
+    y.value_counts().sort_index().plot.bar(x="Label", y="# Occurences")
+    plot.show()
