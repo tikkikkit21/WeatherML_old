36f196d9f8d327d22830907179b757215c5dcb1f
tikkikkit21 <tikkikkit21@gmail.com>
2024-02-04 18:19:13 -0500

    Change training back from class to script

--- train.py
+++ train.py
@@ -12,45 +12,40 @@ def time_to_int(dateString):
     dateInt = int(dateObj.time().strftime('%H%M'))
     return dateInt
 
-class WeatherML:
-    def __init__(self):
-        self.model = LogisticRegression(solver="liblinear", multi_class="ovr")
-        self.labels = ["Date", "Temp", "FeelsLike", "Humidity", "UV", "Wind", "Label", "Rain?"]
-        self.scaler = StandardScaler()
+model = LogisticRegression(solver="liblinear", multi_class="ovr")
+labels = ["Date", "Temp", "FeelsLike", "Humidity", "UV", "Wind", "Label", "Rain?"]
+scaler = StandardScaler()
 
-    def train(self):
-        # prepare dataset
-        data = pd.read_csv("weather_data_v1.csv", names=self.labels, skiprows=1)
+# prepare dataset
+data = pd.read_csv("weather_data_v1.csv", names=labels, skiprows=1)
+data["Date"] = data["Date"].apply(lambda d: time_to_int(d))
+data["Humidity"] = data["Humidity"].apply(lambda h: float(h.strip('%'))/100)
+data = data.drop("Rain?", axis=1)
 
-        data["Date"] = data["Date"].apply(lambda d: time_to_int(d))
-        data["Humidity"] = data["Humidity"].apply(lambda h: float(h.strip('%'))/100)
-        data = data.drop("Rain?", axis=1)
+# split into test/train
+x = data[data.columns.tolist()[:-1]]
+y = data["Label"]
 
-        # split into test/train
-        x = data[data.columns.tolist()[:-1]]
-        y = data["Label"]
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.1, random_state=1)    
 
-        x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.1, random_state=1)    
+# standardize
+scaler.fit(x_train)
+x_train = scaler.transform(x_train)
+x_test = scaler.transform(x_test)
 
-        # standardize
-        self.scaler.fit(x_train)
-        x_train = self.scaler.transform(x_train)
-        x_test = self.scaler.transform(x_test)
+# train
+model.fit(x_train, y_train)
 
-        # train
-        self.model.fit(x_train, y_train)
+# score model
+print("Train accuracy:", percent(model.score(x_train, y_train)))
+print("Test accuracy:", percent(model.score(x_test, y_test)))
 
-        # score model
-        print("Train accuracy:", percent(self.model.score(x_train, y_train)))
-        print("Test accuracy:", percent(self.model.score(x_test, y_test)))
-        print()
+# def predict(*args):
+#     now = datetime.now().time().strftime('%H%M')
+#     args = [int(now)] + list(args)
 
-    def predict(self, *args):
-        now = datetime.now().time().strftime('%H%M')
-        args = [int(now)] + list(args)
-
-        df = pd.DataFrame([args], columns=self.labels[:6])
-        sample = self.scaler.transform(df)
-        
-        print("Prediction:", self.model.predict(sample))
-        print("Confidence:", percent(max(self.model.predict_proba(sample)[0])))
+#     df = pd.DataFrame([args], columns=labels[:6])
+#     sample = scaler.transform(df)
+    
+#     print("Prediction:", model.predict(sample))
+#     print("Confidence:", percent(max(model.predict_proba(sample)[0])))
