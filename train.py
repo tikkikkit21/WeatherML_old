90d12018ade6d0f3d680a2e6ade997e968e58f86
tikkikkit21 <tikkikkit21@gmail.com>
2024-04-03 20:29:53 -0400

    Refactor train.py to use experiments

--- train.py
+++ train.py
@@ -10,7 +10,7 @@ import joblib
 from util import time_to_int, percent
 
 # check for provided version in cline args
-with open('data/version_info.json', 'r') as file:
+with open('data/experiment_config.json', 'r') as file:
     config = json.load(file)
 
 if len(sys.argv) == 1:
@@ -22,17 +22,16 @@ else:
         exit()
     elif not arg.isdigit():
         print('Version needs to be a number!')
-
-    VERSION = f'v{arg}'
+    VERSION = f'exp{arg}'
 
 try:
     config = config[VERSION]
 except KeyError:
-    print(f'Dataset {VERSION} does not exist')
+    print(f'Experiment {VERSION} does not exist')
     exit()
 
 RESULTS_DIR = 'results'
-DATA_CSV = f'data/weather_data_{VERSION}.csv'
+DATA_CSV = f'data/{config["dataset"]}'
 
 if __name__ == '__main__':
     model = LogisticRegression(solver='liblinear', multi_class='ovr')
